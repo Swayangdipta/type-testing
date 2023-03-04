e6ca0af0f1b6603ee31ccc28696f6c74cb44cf47
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-03-04 11:33:02 +0530

    Added animation for timer and added site icons

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,15 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { TimeContext } from '../TimeContext.'
+import { ControlContext } from '../ControlContext'
 
 const Timer = () => {
     const [time,setTime] = useContext(TimeContext)
+    const [control,setControl] = useContext(ControlContext)
+
+    useEffect(()=>{
+      document.getElementById('overlay').style.animation = `spinOverlay ${control.totalTime}s linear infinite`
+    },[])
 
   return (
     <div className='w-full h-full flex items-center justify-center'>
-        <div className='w-[150px] h-[150px] rounded-full border-[1px] bg-zinc-800 border-zinc-600 text-zinc-100 text-[24px] select-none flex items-center justify-center'>
+        <div className='w-[150px] overflow-hidden h-[150px] animate-pulse-slow rounded-full border-[1px] bg-zinc-800 border-zinc-600 text-zinc-100 text-[24px] select-none flex items-center justify-center relative top-0'>
+          <div id='overlay' className=' z-0 w-[300px] h-[300px] rounded-[150px] bg-cyan-500 absolute top-[150px] animate-spin-slow'></div>
+          <p className='z-10'>
             {
                 time.elapsedTime > 60 ? (Math.floor(time.elapsedTime / 60)+"min") : (time.elapsedTime+"secs")
-            }            
+            }              
+          </p>
+          
         </div>
     </div>
   )
